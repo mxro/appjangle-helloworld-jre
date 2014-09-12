d91ce8f1e806fac80b2125bb129c0437f0d5790f
mxro <mxro@nowhere.com>
2014-09-12 10:53:41 +1000

    Fixing bug that sessions are not properly closed -> Caused by missing shutdown for persistence connection -> Implementing a simple counter which keeps track of how many sessions are active. If the counter goes to zero, automatically release global persistence connection Autocommit Fri Sep 12 10:53:41 EST 2014

---
+++ appjangle-helloworld-jre/src/HelloWorld.java
@@ -0,0 +1,19 @@
+import io.nextweb.Query;
+import io.nextweb.Session;
+
+import com.appjangle.jre.AppjangleJre;
+
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		Session session = AppjangleJre.createSession();
+		
+		Query hello = session.seed().setValue("Hello, World!");
+		
+		System.out.println("Here: "+hello.get().uri());
+		
+		session.close().get();
+	}
+
+}
